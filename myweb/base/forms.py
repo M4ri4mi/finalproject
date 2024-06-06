563c4db533593538d601b6ef18e2e110a9ec04c9
Mariam pirosmanashvili <mariami@Mariams-MacBook-Pro.local>
2024-06-06 16:06:50 +0400

    Scheduled added for no reason

--- myweb/base/forms.py
+++ myweb/base/forms.py
@@ -1,7 +1,9 @@
+# base/forms.py
 from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'due_date']
+        fields = ['title', 'description', 'due_date', 'scheduled_date']  # Add 'scheduled_date'
+
