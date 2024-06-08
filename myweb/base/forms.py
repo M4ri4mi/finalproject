dfd3283b63a053a416fb7d701934fc43cdaf84f8
Mariam pirosmanashvili <mariami@Mariams-MacBook-Pro.local>
2024-06-08 23:08:45 +0400

    Calendar added for the project

--- myweb/base/forms.py
+++ myweb/base/forms.py
@@ -40,6 +40,9 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'description', 'due_date']
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'date'})
+        }
 
 
 
